fb4e9675b3c1b5d9336c8926040c777c5d65fc72
Ellie Vrankovich <ellievrank@gmail.com>
2025-12-02 20:56:22 +0800

    Began creating program

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print('Hello, World!')
+import time
+class Bear:
+    def __init__(self, hunger, happiness, name) -> None:
+        self.hunger = hunger
+        self.happiness = happiness
+        self.name = name
+    
+    def display(self):
+        output = f"{self.name} {self.hunger} {self.happiness} {self.get_image()}"
+        print(output, end="\r")
+    
+    def update(self):
+        self.hunger -= 5
+        self.happiness -= 3
+
+    def get_image(self):
+        resting = "ʕ •ᴥ•ʔ"
+        return resting
+
+
+# --- MAIN PROGRAM --- 
+bear_name = input("Choose a name for your bear: ")
+bear = Bear(100, 100, bear_name)
+
+while True:
+    bear.update()
+    bear.display()
+    time.sleep(3)
